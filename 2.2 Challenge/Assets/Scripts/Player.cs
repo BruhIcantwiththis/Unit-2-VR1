f8991c5717e74d3a8936a6358417a913975ead81
BruhIcantwiththis <mtaravella@cherrycreekschools.org>
2022-10-11 11:20:30 -0600

    Added 2.2 and 2.3 challange

---
+++ 2.2 Challenge/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int Lives = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
