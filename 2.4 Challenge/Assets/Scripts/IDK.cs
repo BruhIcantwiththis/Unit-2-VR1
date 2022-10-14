3c3ffe542dd4ec302739546039038989ee794edc
BruhIcantwiththis <mtaravella@cherrycreekschools.org>
2022-10-14 10:38:30 -0600

    Added scene and collider to 2.4

---
+++ 2.4 Challenge/Assets/Scripts/IDK.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDK : MonoBehaviour
+{
+    int hearts = 3;
+    Rigidbody2D player
+        = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(hearts);
+        Debug.Log(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
